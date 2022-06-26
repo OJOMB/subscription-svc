559fbad8f3685392ca485b6a5030ced7cb169052
Oscar Bardrick <oscarm-b@tutamail.com>
2022-06-26 18:15:59 +0200

    add extra users

--- db/relational/dummyData.sql
+++ db/relational/dummyData.sql
@@ -12,22 +12,25 @@ INSERT INTO users VALUES
     ('Yh4WkMFE0MTZnUjD9aYFy','Sven','Smith','rempel.ulices@example.org','2006-04-18','2021-11-28 00:00:00','2021-11-28 00:00:00'),
     ('yne6EtUQ9azjDXGLGZFs8','Claudie','Pouros','derrick.schroeder@example.org','2018-01-24','2021-11-28 00:00:00','2021-11-28 00:00:00'),
     ('521Qlk96BbJoGKseV1nPZ','Vergie','Carroll','ntillman@example.com','1999-04-25','2021-11-28 00:00:00','2021-11-28 00:00:00'),
-    /* the following user has no subscription-plan */
-    ('qo6_0keqGKqDA9EB5obql','Jeffrey','Jordan','jjordan123@example.com','1991-04-23','2021-11-28 00:00:00','2021-11-28 00:00:00');
+    /* the following users have no subscription-plan */
+    ('qo6_0keqGKqDA9EB5obql','Jeffrey','Jordan','jjordan123@example.com','1991-04-23','2021-11-28 00:00:00','2021-11-28 00:00:00'),
+    ('MFE0MTZqGKqDA9EB5obql','Ben','Fisher','benf@example.com','1967-04-23','2021-11-28 00:00:00','2021-11-28 00:00:00');
 
 INSERT INTO vouchers VALUES
     ('10-percent-off','PERCENTAGE', 10.00, '2021-11-28 00:00:00', '2025-05-20 00:00:00', 5, '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
-    ('20-off','FIXED_AMOUNT', 20.00, '2021-11-28 00:00:00', '2025-05-20 00:00:00', 5, '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
+    ('10-off','FIXED_AMOUNT', 10.00, '2021-11-28 00:00:00', '2025-05-20 00:00:00', 5, '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('im-an-expired-voucher','FIXED_AMOUNT', 20.00, '2021-11-28 00:00:00', '2021-11-28 00:01:00', 5, '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('im-a-used-up-voucher','FIXED_AMOUNT', 20.00, '2021-11-28 00:00:00', '2025-05-20 00:00:00', 0, '2021-11-28 00:00:00', '2021-11-28 00:00:00');
 
 /* here vouchers only apply to the 30 day products */
 INSERT INTO products_vouchers VALUES
     ('7Yn_IvvYsfkeo7-ysixd7', '10-percent-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
+    ('7Yn_IvvYsfkeo7-ysixd7', '10-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('7Yn_IvvYsfkeo7-ysixd7', '20-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('7Yn_IvvYsfkeo7-ysixd7', 'im-an-expired-voucher', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('7Yn_IvvYsfkeo7-ysixd7', 'im-a-used-up-voucher', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('Ft2GgLRgN3FbMveklTy-W', '10-percent-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
+    ('Ft2GgLRgN3FbMveklTy-W', '10-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('Ft2GgLRgN3FbMveklTy-W', '20-off', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('Ft2GgLRgN3FbMveklTy-W', 'im-an-expired-voucher', '2021-11-28 00:00:00', '2021-11-28 00:00:00'),
     ('Ft2GgLRgN3FbMveklTy-W', 'im-a-used-up-voucher', '2021-11-28 00:00:00', '2021-11-28 00:00:00');
